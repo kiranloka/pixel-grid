aa08e6eca09c7aa15d4779fa5989db4667604126
kiranLoka <kirankumarloka@gmail.com>
2026-02-10 00:19:51 +0530

    pixel grid v1

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,20 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Inter, Playfair_Display } from "next/font/google";
+
 import "./globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const playfair_display = Playfair_Display({
+  variable: "--font-playfair-display",
+  subsets: ["latin"],
+});
+
+const inter = Inter({
+  variable: "--font-inter",
   subsets: ["latin"],
 });
 
@@ -23,9 +34,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="dark" suppressHydrationWarning>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} ${inter.variable} ${playfair_display.variable} antialiased`}
       >
         {children}
       </body>
---
+++ fonts.tsx
@@ -0,0 +1,8 @@
+// fonts.ts
+import { Inter, Playfair_Display } from "next/font/google";
+
+export const inter = Inter({ subsets: ["latin"] });
+export const playfair = Playfair_Display({
+  subsets: ["latin"],
+  style: "italic",
+});
